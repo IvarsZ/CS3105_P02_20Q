de3ea66028a06cc6d4db7b22e1dc9acac3fda8ef
iz2 <devnull@localhost>
2013-04-16 03:05:50 +0100

    More tests, still need lots more.

--- src/tests/TwentyQuestionsTest.java
+++ src/tests/TwentyQuestionsTest.java
@@ -1,10 +1,8 @@
 package tests;
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 import model.Answer;
 import model.Round;
@@ -19,14 +17,14 @@ public class TwentyQuestionsTest {
 
 	@Test
 	public void testGuessAll() throws IOException {
-		
+
 		TwentyQuestionsView view = new TwentyQuestionsView("Part1-Input1.in");
 		TwentyQuestionsModel model = view.getModel();
-		
+
 		// Check that all concepts are correctly guessed.
 		String[] concepts = new String[]{"Wristwatch", "Candy", "Chair", "Pumpkin", "Ant", "Oyster", "Human"};
 		for (int i = 0; i < concepts.length; i++) {
-			
+
 			Round game = new Round(model);
 			game.addAnswer(new Answer(model.getQuestions().get(2), i % 2));
 			game.addAnswer(new Answer(model.getQuestions().get(1), (i % 4) / 2));
@@ -35,25 +33,30 @@ public class TwentyQuestionsTest {
 		}
 	}
 
-	/*
 	@Test
-	public void q20Test2() throws FileNotFoundException {
+	public void addConceptTest() throws IOException {
 
-		// Read the twenty questions file, train the network and get output.
-		File inFile = new File("Part1-Input2.in");
-		Scanner in = new Scanner(inFile);
-		TwentyQuestionsModel q20Model = TwentyQuestionsView.read(in);
-		in.close();
-		q20Model.train();
+		TwentyQuestionsView view = new TwentyQuestionsView("Part1-Input1.in");
+		TwentyQuestionsModel model = view.getModel();
+
+		// Add pig which answers yes to all questions.
+		ArrayList<Answer> answers = new ArrayList<Answer>();
+		for (int i = 0; i < model.getQuestions().size(); i++) {
+			answers.add(new Answer(model.getQuestions().get(i), 1));
+		}
+		model.addConcept("Pig", answers);
 
 		// Check that all concepts are correctly guessed.
-		assertEquals("Pumpkin", q20Model.guessConcept(new double[]{0, 1, 1}).getName());
-		assertEquals("Chair", q20Model.guessConcept(new double[]{0, 1, 0}).getName());
-		assertEquals("Wristwatch", q20Model.guessConcept(new double[]{0, 0, 0}).getName());
-		assertEquals("Ant", q20Model.guessConcept(new double[]{1, 0, 0}).getName());
-		assertEquals("Oyster", q20Model.guessConcept(new double[]{1, 0, 1}).getName());
+		String[] concepts = new String[]{"Wristwatch", "Candy", "Chair", "Pumpkin", "Ant", "Oyster", "Human", "Pig"};
+		for (int i = 0; i < concepts.length; i++) {
+
+			Round game = new Round(model);
+			game.addAnswer(new Answer(model.getQuestions().get(2), i % 2));
+			game.addAnswer(new Answer(model.getQuestions().get(1), (i % 4) / 2));
+			game.addAnswer(new Answer(model.getQuestions().get(0), i/4));
+			assertEquals(concepts[i], game.getGuessedConcept().getName());
+		}
 	}
-	*/
 
 	/**
 	 * Less questions test.
@@ -83,35 +86,6 @@ public class TwentyQuestionsTest {
 	}
 
 	@Test
-	public void addConceptTest() throws FileNotFoundException {
-
-		// Read the twenty questions file, train the network and get output.
-		File inFile = new File("Part1-Input1.in");
-		Scanner in = new Scanner(inFile);
-		TwentyQuestionsModel q20Model = TwentyQuestionsView.read(in);
-		in.close();
-		q20Model.train();
-
-		ArrayList<Question> questions = q20Model.getQuestions();
-		ArrayList<Answer> answers = new ArrayList<Answer>();
-		for (int i = 0; i < questions.size(); i++) {
-
-			answers.add(new Answer(questions.get(i), 1));
-		}
-		q20Model.addConcept("Pig", answers);
-
-		// Check that all concepts are correctly guessed.
-		assertEquals("Pumpkin", q20Model.guessConcept(new double[]{0, 1, 1}).getName());
-		assertEquals("Pig", q20Model.guessConcept(new double[]{1, 1, 1}).getName());
-		assertEquals("Chair", q20Model.guessConcept(new double[]{0, 1, 0}).getName());
-		assertEquals("Wristwatch", q20Model.guessConcept(new double[]{0, 0, 0}).getName());
-		assertEquals("Candy", q20Model.guessConcept(new double[]{0, 0, 1}).getName());
-		assertEquals("Ant", q20Model.guessConcept(new double[]{1, 0, 0}).getName());
-		assertEquals("Oyster", q20Model.guessConcept(new double[]{1, 0, 1}).getName());
-		assertEquals("Human", q20Model.guessConcept(new double[]{1, 1, 0}).getName());
-	}
-
-	@Test
 	public void addQuestionTest() throws IOException {
 
 		// Read the twenty questions file, train the network and get output.
@@ -137,28 +111,28 @@ public class TwentyQuestionsTest {
 		// Check that all concepts are correctly guessed for all possible inputs.
 		assertEquals("Pumpkin", q20Model.guessConcept(new double[]{0, 1, 1, 0}).getName());
 		assertEquals("Cabbage", q20Model.guessConcept(new double[]{0, 1, 1, 1}).getName());
-		
+
 		assertEquals("Pig", q20Model.guessConcept(new double[]{1, 1, 1, 0}).getName());
 		assertEquals("Pig", q20Model.guessConcept(new double[]{1, 1, 1, 1}).getName());
-		
+
 		assertEquals("Chair", q20Model.guessConcept(new double[]{0, 1, 0, 0}).getName());
 		assertEquals("Chair", q20Model.guessConcept(new double[]{0, 1, 0, 1}).getName());
-		
+
 		assertEquals("Wristwatch", q20Model.guessConcept(new double[]{0, 0, 0, 0}).getName());
 		assertEquals("Wristwatch", q20Model.guessConcept(new double[]{0, 0, 0, 1}).getName());
-		
+
 		assertEquals("Candy", q20Model.guessConcept(new double[]{0, 0, 1, 0}).getName());
 		assertEquals("Candy", q20Model.guessConcept(new double[]{0, 0, 1, 1}).getName());
-		
+
 		assertEquals("Ant", q20Model.guessConcept(new double[]{1, 0, 0, 0}).getName());
 		assertEquals("Ant", q20Model.guessConcept(new double[]{1, 0, 0, 1}).getName());
-		
+
 		assertEquals("Oyster", q20Model.guessConcept(new double[]{1, 0, 1, 0}).getName());
 		assertEquals("Oyster", q20Model.guessConcept(new double[]{1, 0, 1, 1}).getName());
-		
+
 		assertEquals("Human", q20Model.guessConcept(new double[]{1, 1, 0, 0}).getName());
 		assertEquals("Human", q20Model.guessConcept(new double[]{1, 1, 0, 1}).getName());
 	}
-	*/
+	 */
 
 }
